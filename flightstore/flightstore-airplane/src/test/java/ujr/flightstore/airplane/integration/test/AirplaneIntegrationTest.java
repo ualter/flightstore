36a408b84edf31f04b25e4f8131b7b595279f04a
Ualter Azambuja Junior <ualter.junior@gmail.com>
2019-10-18 11:41:15 +0200

    .

--- flightstore/flightstore-airplane/src/test/java/ujr/flightstore/airplane/integration/test/AirplaneIntegrationTest.java
+++ flightstore/flightstore-airplane/src/test/java/ujr/flightstore/airplane/integration/test/AirplaneIntegrationTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.IfProfileValue;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
