ff05b804bc808cb5cb1d695d4721b36590336fdb
Ualter Azambuja Junior <ualter.junior@gmail.com>
2019-10-21 09:16:35 +0200

    .

--- flightstore/dockers/registry/start.sh
+++ flightstore/dockers/registry/start.sh
@@ -1,17 +1,14 @@
-#!bin/bash
+#!/bin/bash
 mkdir -p /Users/ualter/Temp/docker-registry-htpasswd
-docker run --rm --entrypoint htpasswd registry:2 -Bbn ualter 1234 > /Users/ualter/Temp/docker-registry-htpasswd
-echo Start Docker Compose
-echo  ********************
+docker run --rm --entrypoint htpasswd registry:2 -Bbn ualter 1234 > /Users/ualter/Temp/docker-registry-htpasswd/htpasswd
+echo "Start Docker Compose"
+echo "********************"
 docker-compose up -d
 echo
-echo Lets follow the Logs (Ctrl+C)
-echo ****************************** 
+echo "Lets follow the Logs (Ctrl+C)"
+echo *******************************
 docker-compose logs -f --tail=30
 echo
-echo Ok!
+echo "Ok!"
 echo
 
-.env
-#MY_REGISTRY_CERTS=certs
-#REGISTRY_AUTH_PATH=/Users/ualter/Temp/docker-registry-htpasswd
