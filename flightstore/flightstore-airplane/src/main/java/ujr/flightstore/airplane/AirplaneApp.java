cdb22a459ac93e5370d3825548d5bfcb5a737ca9
Ualter Azambuja Junior <ualter.junior@gmail.com>
2019-11-05 13:16:47 +0100

    .

--- flightstore/flightstore-airplane/src/main/java/ujr/flightstore/airplane/AirplaneApp.java
+++ flightstore/flightstore-airplane/src/main/java/ujr/flightstore/airplane/AirplaneApp.java
@@ -3,6 +3,7 @@ package ujr.flightstore.airplane;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.kubernetes.config.reload.ConfigReloadAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
@@ -10,7 +11,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import ujr.flightstore.config.CacheConfigProperties;
 import ujr.flightstore.config.MysqlConfigProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ConfigReloadAutoConfiguration.class)
 @ComponentScan(basePackages = "ujr.flightstore")
 @EnableEncryptableProperties
 @EnableConfigurationProperties({MysqlConfigProperties.class, CacheConfigProperties.class})
